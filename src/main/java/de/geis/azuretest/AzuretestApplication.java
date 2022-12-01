8549544c6d60f8a1fe2e7166c9afab7e14423b53
DivContainer <David@Isecke.de>
2022-12-01 21:10:02 +0100

    Initial commit

---
+++ src/main/java/de/geis/azuretest/AzuretestApplication.java
@@ -0,0 +1,23 @@
+package de.geis.azuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@SpringBootApplication
+public class AzuretestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuretestApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String getTestMessage() {
+		return "YEAHRRRR";
+	}
+
+}
---
+++ src/test/java/de/geis/azuretest/AzuretestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.geis.azuretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuretestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
